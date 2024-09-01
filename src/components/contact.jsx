1b585ded1e9f67e152c793c1554e51ec66b28587
naveenchinnadurai <dev.iamnaveen@gmail.com>
2024-09-01 20:10:47 +0530

    updated:home

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,18 +5,18 @@ import { LuMailOpen as Mail } from 'react-icons/lu';
 
 function Contact() {
     return (
-        <div className="flex flex-col md:flex-row bg-purple-600 w-4/5 md:w-5/6 mx-auto p-5 items-center justify-center relative">
+        <div className="flex flex-col md:flex-row bg-purple-600 w-4/5 md:w-5/6 mx-auto p-5 !py-10 items-center justify-center relative">
             <div className="flex flex-col items-center justify-center w-full md:w-1/2 p-5 gap-3">
                 <h1 className="text-3xl font-semibold text-white mb-4">Contact us</h1>
                 <div className="flex flex-col items-center gap-4 w-full">
                     {contactInfo.map((e) => (
-                        <ContactCard
-                            key={e.id}
-                            icon={e.icon}
-                            title={e.title}
-                            text={e.text}
-                            className="flex-1"
-                        />
+                        <div className='flex items-center gap-4 p-2'>
+                            <span className="text-2xl">{e.icon}</span>
+                            <div className="flex flex-col">
+                                <h4 className="text-lg font-semibold">{e.title}</h4>
+                                <p className="text-sm">{e.text}</p>
+                            </div>
+                        </div>
                     ))}
                 </div>
             </div>
@@ -46,17 +46,5 @@ const contactInfo = [
         text: 'sectrichengode@secteg.co.in'
     }
 ];
-
-function ContactCard(props) {
-    return (
-        <div className={`flex items-center gap-4 p-2 ${props.className}`}>
-            <span className="text-2xl">{props.icon}</span>
-            <div className="flex flex-col">
-                <h4 className="text-lg font-semibold">{props.title}</h4>
-                <p className="text-sm">{props.text}</p>
-            </div>
-        </div>
-    );
-}
 
 export default Contact;
